d9c89a6f28b7e852bf5fde9b0397daef38672324
s.vasiliev <stan.vasiljev@gmail.com>
2019-09-05 19:38:19 +0300

    Move hibernate db connection settings into conf/hibernate.db.properties

--- src/main/java/utils/HibernateSessionFactoryUtil.java
+++ src/main/java/utils/HibernateSessionFactoryUtil.java
@@ -7,13 +7,25 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class HibernateSessionFactoryUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final String HibernatePropertiesPath = "conf/hibernate.db.properties";
 
     public static SessionFactory buildSessionFactory() {
+        Properties externalDBConfig = new Properties();
+        try (InputStream inS = new FileInputStream(HibernatePropertiesPath)) {
+            externalDBConfig.load(inS);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
         try {
             StandardServiceRegistry standartRegistry = new StandardServiceRegistryBuilder()
-                    .configure("hibernate.cfg.xml").build();
+                    .applySettings(externalDBConfig).configure("hibernate.cfg.xml").build();
             Metadata metadata= new MetadataSources((standartRegistry)).getMetadataBuilder().build();
             return metadata.getSessionFactoryBuilder().build();
         } catch (HibernateException he){
